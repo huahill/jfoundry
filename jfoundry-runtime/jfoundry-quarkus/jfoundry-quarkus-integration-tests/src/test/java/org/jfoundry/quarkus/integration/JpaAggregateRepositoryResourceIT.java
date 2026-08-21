84c5f909922d9886d1ec90f437fb1ce0736b658b
Huang Xiao <youngledo@qq.com>
2026-08-22 02:03:52 +0800

    test(quarkus): configure PostgreSQL for native integration tests

--- jfoundry-runtime/jfoundry-quarkus/jfoundry-quarkus-integration-tests/src/test/java/org/jfoundry/quarkus/integration/JpaAggregateRepositoryResourceIT.java
+++ jfoundry-runtime/jfoundry-quarkus/jfoundry-quarkus-integration-tests/src/test/java/org/jfoundry/quarkus/integration/JpaAggregateRepositoryResourceIT.java
@@ -1,11 +1,13 @@
 package org.jfoundry.quarkus.integration;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
 
 @QuarkusIntegrationTest
+@TestProfile(PostgreSqlIntegrationTestProfile.class)
 class JpaAggregateRepositoryResourceIT {
 
     @Test
