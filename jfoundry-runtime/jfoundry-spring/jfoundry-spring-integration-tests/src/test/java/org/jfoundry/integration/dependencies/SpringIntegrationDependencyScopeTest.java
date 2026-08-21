3680e0068f8f0223b973e3236dd173194d3a8a0c
Huang Xiao <youngledo@qq.com>
2026-08-21 10:49:04 +0800

    fix(spring): keep JPA support on native runtime classpath

---
+++ jfoundry-runtime/jfoundry-spring/jfoundry-spring-integration-tests/src/test/java/org/jfoundry/integration/dependencies/SpringIntegrationDependencyScopeTest.java
@@ -0,0 +1,52 @@
+package org.jfoundry.integration.dependencies;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringIntegrationDependencyScopeTest {
+
+    private static final String MAVEN_POM_NAMESPACE = "http://maven.apache.org/POM/4.0.0";
+
+    @Test
+    void doesNotOverrideJpaSupportRuntimeDependencyWithTestScope() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document document = factory.newDocumentBuilder()
+                .parse(modulePom().toFile());
+        NodeList dependencies = document.getElementsByTagNameNS(MAVEN_POM_NAMESPACE, "dependency");
+
+        assertThat(hasDirectDependency(dependencies, "jfoundry-jpa-spring-boot-support")).isFalse();
+    }
+
+    private Path modulePom() {
+        Path current = Path.of("").toAbsolutePath().normalize();
+        while (current != null) {
+            Path candidate = current.resolve(
+                    "jfoundry-runtime/jfoundry-spring/jfoundry-spring-integration-tests/pom.xml");
+            if (Files.isRegularFile(candidate)) {
+                return candidate;
+            }
+            current = current.getParent();
+        }
+        throw new IllegalStateException("Cannot locate Spring integration test POM");
+    }
+
+    private boolean hasDirectDependency(NodeList dependencies, String artifactId) {
+        for (int index = 0; index < dependencies.getLength(); index++) {
+            Element dependency = (Element) dependencies.item(index);
+            NodeList artifactIds = dependency.getElementsByTagNameNS(MAVEN_POM_NAMESPACE, "artifactId");
+            if (artifactIds.getLength() > 0 && artifactId.equals(artifactIds.item(0).getTextContent())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
